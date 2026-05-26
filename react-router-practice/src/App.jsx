96ae3af2be5c1096cb20f2ae8f0c161e5c4bf63f
Abu Talha <talha007abu@gmail.com>
2026-05-27 00:24:29 +0600

    simple nav link and multipage routing added

--- react-router-practice/src/App.jsx
+++ react-router-practice/src/App.jsx
@@ -1,21 +1,42 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom";
 
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
+import Services from "./pages/Services";
 
 function App() {
   return (
     <div>
-      <Routes>
 
+      <nav>
+        <Link to="/">Home</Link>
+
+        <br />
+
+        <Link to="/about">About</Link>
+
+        <br />
+
+        <Link to="/contact">Contact</Link>
+
+        <br />
+
+        <Link to="/services">Services</Link>
+      </nav>
+
+      <hr />
+
+      <Routes>
         <Route path="/" element={<Home />} />
 
         <Route path="/about" element={<About />} />
 
         <Route path="/contact" element={<Contact />} />
 
+        <Route path="/services" element={<Services />} />
       </Routes>
+
     </div>
   );
 }
---
+++ react-router-practice/src/pages/Services.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Services = () => {
+  return (
+    <div>
+      <h1>Services</h1>
+      <p>Welcome to our services page!</p>
+    </div>
+  )
+}
+
+export default Services
